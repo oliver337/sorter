09276ebd11c47c9b2b0d2d861c4775285b1ee24a
oliver337 <43149645+oliver337@users.noreply.github.com>
2018-09-23 23:05:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,74 @@
 class Sorter {
-  constructor() {
-    // your implementation
+   
+  constructor(a=[], kol=0) {
+    this.a=a;
+    this.kol=kol;
   }
-
+  
   add(element) {
-    // your implementation
+    this.a.push(element);
+    this.kol=this.kol+1;
   }
 
   at(index) {
-    // your implementation
-  }
+   return this.a[index];
+    }
 
   get length() {
-    // your implementation
+    //console.log(this.kol);
+    return this.kol;
+
   }
 
   toArray() {
-    // your implementation
+    console.log(this.a);
+    return this.a;
   }
 
   sort(indices) {
+    var arr=[];
+
+    for (var i = 0, endI = indices.length - 1; i < endI; i++) {
+      for (var j = 0, endJ = endI - i; j < endJ; j++) {
+
+          if (indices[j] > indices[j + 1]) {
+              var swap = indices[j];
+
+              indices[j] = indices[j + 1];
+              indices[j + 1] = swap;
+          }
+      }
+  }
+
+    for (var i=0; i<indices.length; i++)
+    {
+      arr[i]=this.a[indices[i]];
+    }
+    
+        for (var i = 0, endI = arr.length - 1; i < endI; i++) {
+        for (var j = 0, endJ = endI - i; j < endJ; j++) {
+
+            if (arr[j] > arr[j + 1]) {
+                var swap = arr[j];
+
+                arr[j] = arr[j + 1];
+                arr[j + 1] = swap;
+            }
+        }
+    }
+    for (var i=0; i<indices.length; i++)
+    {
+      this.a[indices[i]]=arr[i];
+    }
+
+    return 0;
     // your implementation
   }
 
   setComparator(compareFunction) {
     // your implementation
   }
+  
 }
 
 module.exports = Sorter;
